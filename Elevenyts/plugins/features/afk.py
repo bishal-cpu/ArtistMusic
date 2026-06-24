13e24bacc97f0121c6a4c9b6cf0945775a09f855
bishal-cpu <bishalk1299@gmail.com>
2026-06-24 16:18:23 +0530

    Update afk.py

--- Elevenyts/plugins/features/afk.py
+++ Elevenyts/plugins/features/afk.py
@@ -8,6 +8,7 @@ import asyncio
 # IMPORTANT: change "bot" to your main file name
 # Example: from main import db
 # -------------------------------------------------
+print("AFK PLUGIN LOADED")
 from Elevenyts import db
 
 afk_col = db.db.afk
